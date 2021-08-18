0db02cad8bb77972a6a57309c9dc8a1d0b725a86
Mike <5188083@gmail.com>
2021-08-18 23:25:00 +0200

    second death)

--- trafficLights/ViewController.swift
+++ trafficLights/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redRoundShow: UIView!
+    
+    @IBOutlet var yellouRounShow: UIView!
+    
+    @IBOutlet var greenRoundShow: UIView!
+    
+    @IBOutlet var startBottomPress: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redRoundShow.layer.cornerRadius = 63
+    
+        yellouRounShow.layer.cornerRadius = 63
+        
+        greenRoundShow.layer.cornerRadius = 63
+        
     }
-
-
+    @IBAction func startPressFirstTime() {
+        redRoundShow.alpha = 1
+       
+    }
+    
+   
+    
 }
 
